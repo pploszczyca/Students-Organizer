327ab0907800a1a1378273758a7d11b8f0940823
pploszczyca <piotrus.plo@gmail.com>
2023-02-05 15:56:26 +0100

    Added Student to domain

--- backend/src/main/scala/com/pp/students_organizer_backend/domain/StudentEntity.scala
+++ backend/src/main/scala/com/pp/students_organizer_backend/domain/StudentEntity.scala
@@ -1,7 +1,37 @@
 package com.pp.students_organizer_backend.domain
 
+import com.pp.students_organizer_backend.domain.errors.ValidationError
+import com.pp.students_organizer_backend.utils.validators.ValidateEmptyString.*
+
+import java.util.UUID
+
 case class StudentEntity(
-    id: Int,
-    name: String,
+    id: StudentId,
+    name: StudentName,
     assignments: List[AssignmentEntity] = List.empty
 )
+object StudentEntity:
+  def create(
+      name: String,
+      assignments: List[AssignmentEntity] = List.empty
+  ): Either[ValidationError, StudentEntity] =
+    for
+      studentId <- StudentId.create
+      studentName <- StudentName.create(name)
+    yield StudentEntity(
+      id = studentId,
+      name = studentName,
+      assignments = assignments
+    )
+
+case class StudentId(value: UUID)
+object StudentId:
+  def create: Either[ValidationError, StudentId] =
+    Right(StudentId(UUID.randomUUID()))
+
+case class StudentName(value: String)
+object StudentName:
+  def create(value: String): Either[ValidationError, StudentName] =
+    value
+      .validateEmptyString("Student Name can't be null")
+      .map(StudentName.apply)
