1fc17d60e79555705ec3e499a5b5b77fd07a1f6c
pploszczyca <piotrus.plo@gmail.com>
2023-02-05 16:14:54 +0100

    Added SubjectEntity to domain

--- backend/src/main/scala/com/pp/students_organizer_backend/domain/SubjectEntity.scala
+++ backend/src/main/scala/com/pp/students_organizer_backend/domain/SubjectEntity.scala
@@ -1,7 +1,37 @@
 package com.pp.students_organizer_backend.domain
 
+import com.pp.students_organizer_backend.domain.errors.ValidationError
+import com.pp.students_organizer_backend.utils.validators.ValidateEmptyString.*
+
+import java.util.UUID
+
 case class SubjectEntity(
-    id: Long,
-    name: String,
+    id: SubjectId,
+    name: SubjectName,
     assignments: List[AssignmentEntity]
 )
+object SubjectEntity:
+  def create(
+      name: String,
+      assignments: List[AssignmentEntity] = List.empty
+  ): Either[ValidationError, SubjectEntity] =
+    for
+      subjectId <- SubjectId.create
+      subjectName <- SubjectName.create(name)
+    yield SubjectEntity(
+      id = subjectId,
+      name = subjectName,
+      assignments = assignments
+    )
+
+case class SubjectId(value: UUID)
+object SubjectId:
+  def create: Either[ValidationError, SubjectId] =
+    Right(SubjectId(UUID.randomUUID()))
+
+case class SubjectName(value: String)
+object SubjectName:
+  def create(value: String): Either[ValidationError, SubjectName] =
+    value
+      .validateEmptyString("Subject name can't be empty")
+      .map(SubjectName.apply)
