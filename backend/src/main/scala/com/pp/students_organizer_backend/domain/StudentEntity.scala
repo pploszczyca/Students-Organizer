9d707b7673ff6e48aaff3e3d8b9c9edc12016b76
pploszczyca <piotrus.plo@gmail.com>
2023-02-15 22:13:05 +0100

    Added password to StudentEntity

--- backend/src/main/scala/com/pp/students_organizer_backend/domain/StudentEntity.scala
+++ backend/src/main/scala/com/pp/students_organizer_backend/domain/StudentEntity.scala
@@ -8,17 +8,21 @@ import java.util.UUID
 case class StudentEntity(
     id: StudentId,
     name: StudentName,
+    password: StudentPassword,
 )
 object StudentEntity:
   def create(
       name: String,
+      password: String,
   ): Either[ValidationError, StudentEntity] =
     for
       studentId <- StudentId.create
       studentName <- StudentName.create(name)
+      studentPassword <- StudentPassword.create(password)
     yield StudentEntity(
       id = studentId,
       name = studentName,
+      password = studentPassword,
     )
 
 case class StudentId(value: UUID)
@@ -32,3 +36,8 @@ object StudentName:
     value
       .validateEmptyString("Student Name can't be null")
       .map(StudentName.apply)
+
+case class StudentPassword(value: String)
+object StudentPassword:
+  def create(value: String): Either[ValidationError, StudentPassword] =
+    Right(StudentPassword(value))
