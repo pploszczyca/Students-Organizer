907ed3587388730767ed560377c323a85d8975f0
pploszczyca <piotrus.plo@gmail.com>
2023-02-18 12:36:23 +0100

    Added AuthService trait and implementation

--- backend/build.sbt
+++ backend/build.sbt
@@ -3,7 +3,7 @@ val MunitVersion = "0.7.29"
 val LogbackVersion = "1.4.5"
 val MunitCatsEffectVersion = "1.0.7"
 val skunkVersion = "0.5.1"
-val circleVersion = "0.14.3"
+val circleVersion = "0.14.4"
 val scalaTestVersion = "3.2.15"
 val mockitoVersion = "3.2.15.0"
 val jwtVersion = "9.2.0"
@@ -25,6 +25,7 @@ lazy val root = (project in file("."))
       "org.typelevel"   %% "munit-cats-effect-3" % MunitCatsEffectVersion % Test,
       "ch.qos.logback"  %  "logback-classic"     % LogbackVersion,
       "org.tpolecat" %% "skunk-core" % skunkVersion,
+      "io.circe" %% "circe-core" % circleVersion,
       "io.circe" %% "circe-generic" % circleVersion,
       "io.circe" %% "circe-literal" % circleVersion,
       "com.github.jwt-scala" %% "jwt-core" % jwtVersion,
---
+++ backend/src/main/scala/com/pp/students_organizer_backend/services/AuthService.scala
@@ -0,0 +1,55 @@
+package com.pp.students_organizer_backend.services
+
+import cats.effect.kernel.Concurrent
+import cats.syntax.all.*
+import com.pp.students_organizer_backend.domain.{StudentEntity, StudentName}
+import dev.profunktor.auth.jwt.JwtToken
+import dev.profunktor.redis4cats.RedisCommands
+import io.circe
+import io.circe.*
+import io.circe.Encoder.encodeString
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+
+import scala.concurrent.duration.{FiniteDuration, MINUTES}
+
+trait AuthService[F[_]]:
+  def findStudentBy(token: JwtToken): F[Option[StudentEntity]]
+  def findTokenBy(name: StudentName): F[Option[JwtToken]]
+  def insertStudentWithToken(student: StudentEntity, token: JwtToken): F[Unit]
+  def delete(student: StudentEntity, token: JwtToken): F[Unit]
+
+object AuthService:
+  def make[F[_]: Concurrent](
+      redis: RedisCommands[F, String, String]
+  ): AuthService[F] =
+    new AuthService[F]:
+      private val tokenExpiration = FiniteDuration(10, MINUTES)
+
+      override def findStudentBy(token: JwtToken): F[Option[StudentEntity]] =
+        redis
+          .get(token.value)
+          .map {
+            _.flatMap { student =>
+              circe.jawn.decode[StudentEntity](student).toOption
+            }
+          }
+
+      override def findTokenBy(name: StudentName): F[Option[JwtToken]] =
+        redis
+          .get(name.value)
+          .map(_.map(JwtToken.apply))
+
+      override def insertStudentWithToken(
+          student: StudentEntity,
+          token: JwtToken
+      ): F[Unit] =
+        redis.setEx(token.value, student.asJson.noSpaces, tokenExpiration) *>
+          redis.setEx(student.name.value, token.value, tokenExpiration)
+
+      override def delete(
+          student: StudentEntity,
+          token: JwtToken
+      ): F[Unit] =
+        redis.del(token.value) *>
+          redis.del(student.name.value).void
