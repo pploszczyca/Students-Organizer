b521322b26ac2b47c748db9e3641b2106e6f0a05
pploszczyca <piotrus.plo@gmail.com>
2023-03-04 20:08:25 +0100

    Added tests for TaskGateway

--- backend/src/test/scala/com/pp/students_organizer_backend/gateways/task/TaskGatewayTest.scala
+++ backend/src/test/scala/com/pp/students_organizer_backend/gateways/task/TaskGatewayTest.scala
@@ -1,17 +1,17 @@
 package com.pp.students_organizer_backend.gateways.task
 
 import cats.effect.IO
-import com.pp.students_organizer_backend.domain.{StudentId, TaskEntity, TaskId}
+import cats.effect.unsafe.implicits.global
+import com.pp.students_organizer_backend.domain.errors.{AssignmentNotFoundException, ValidationError, ValidationException}
+import com.pp.students_organizer_backend.domain.*
 import com.pp.students_organizer_backend.gateways.task.mappers.{GetTaskResponseMapper, TaskEntityMapper}
+import com.pp.students_organizer_backend.routes_models.task.request.InsertTaskRequest
+import com.pp.students_organizer_backend.routes_models.task.response.GetTaskResponse
 import com.pp.students_organizer_backend.services.{AssignmentService, TaskService}
-import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.{any, anySet}
 import org.mockito.Mockito.{inOrder, verify, when}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.mockito.MockitoSugar.mock
-import cats.effect.unsafe.implicits.global
-import com.pp.students_organizer_backend.domain.errors.{ValidationError, ValidationException}
-import com.pp.students_organizer_backend.routes_models.task.request.InsertTaskRequest
-import com.pp.students_organizer_backend.routes_models.task.response.GetTaskResponse
 
 import java.util.UUID
 
@@ -30,33 +30,16 @@ class TaskGatewayTest extends AnyFlatSpec:
     given getTaskResponseMapper: GetTaskResponseMapper = mock
 
     when(getTaskResponseMapper.map(any())) thenReturn response
-    when(taskService.getAll) thenReturn IO(List(task))
+    when(taskService.getAll(any())) thenReturn IO(List(task))
 
     val actual = tested(
-      taskService = taskService,
+      taskService = taskService
     ).getAll(studentId).unsafeRunSync()
 
-    verify(getTaskResponseMapper).map(task)
+    val inOrderCheck = inOrder(getTaskResponseMapper, taskService)
+    inOrderCheck.verify(taskService).getAll(studentId)
+    inOrderCheck.verify(getTaskResponseMapper).map(task)
     assert(actual == expected)
-  }
-
-  "ON insert" should "insert new task" in {
-    val studentId = mock[StudentId]
-    val request = mock[InsertTaskRequest]
-    val task = mock[TaskEntity]
-
-    given taskEntityMapper: TaskEntityMapper = mock
-
-    when(taskEntityMapper.map(any())) thenReturn Right(task)
-    when(taskService.insert(any())) thenReturn IO.unit
-
-    tested(
-      taskService = taskService,
-    ).insert(request, studentId)
-
-    val inOrderCheck = inOrder(taskEntityMapper, taskService)
-    inOrderCheck.verify(taskEntityMapper).map(request)
-    inOrderCheck.verify(taskService).insert(task)
   }
 
   "ON insert" should "throw validation exception WHEN mapper return error" in {
@@ -78,6 +61,58 @@ class TaskGatewayTest extends AnyFlatSpec:
     assert(actualException == expectedException)
   }
 
+  "ON insert" should "insert new task" in {
+    val studentId = mock[StudentId]
+    val request = mock[InsertTaskRequest]
+    val assignmentId = mock[AssignmentId]
+    val task = fakeTask(
+      assignmentId = assignmentId,
+    )
+    val assignment = mock[AssignmentEntity]
+
+    given taskEntityMapper: TaskEntityMapper = mock
+
+    when(taskEntityMapper.map(any())) thenReturn Right(task)
+    when(assignmentService.get(any(), any())) thenReturn IO(Some(assignment))
+    when(taskService.insert(any())) thenReturn IO.unit
+
+    tested(
+      taskService = taskService,
+      assignmentService = assignmentService,
+    ).insert(request, studentId).unsafeRunSync()
+
+    val inOrderCheck = inOrder(taskEntityMapper, assignmentService, taskService)
+    inOrderCheck.verify(taskEntityMapper).map(request)
+    inOrderCheck.verify(assignmentService).get(assignmentId, studentId)
+    inOrderCheck.verify(taskService).insert(task)
+  }
+
+  "ON insert" should "throw assigment not found exception WHEN assigment is not found" in {
+    val studentId = mock[StudentId]
+    val request = mock[InsertTaskRequest]
+    val assignmentId = mock[AssignmentId]
+    val task = fakeTask(
+      assignmentId = assignmentId,
+    )
+    val expectedException = AssignmentNotFoundException(assignmentId)
+
+    given taskEntityMapper: TaskEntityMapper = mock
+
+    when(taskEntityMapper.map(any())) thenReturn Right(task)
+    when(assignmentService.get(any(), any())) thenReturn IO(None)
+
+    val actualException = intercept[AssignmentNotFoundException] {
+      tested(
+        assignmentService = assignmentService,
+      ).insert(request, studentId).unsafeRunSync()
+    }
+
+    val inOrderCheck = inOrder(taskEntityMapper, assignmentService)
+    inOrderCheck.verify(taskEntityMapper).map(request)
+    inOrderCheck.verify(assignmentService).get(assignmentId, studentId)
+    assert(actualException == expectedException)
+  }
+
   "ON remove" should "remove task" in {
     val studentId = mock[StudentId]
     val id = UUID.fromString("e4ae8cea-dba7-4356-a9d9-f8cc6450e8f7")
@@ -92,14 +127,26 @@ class TaskGatewayTest extends AnyFlatSpec:
     verify(taskService).remove(taskId, studentId)
   }
 
+  private def fakeTask(
+      id: TaskId = mock,
+      name: TaskName = mock,
+      isDone: TaskIsDone = mock,
+      assignmentId: AssignmentId = mock
+  ) = TaskEntity(
+    id = id,
+    name = name,
+    isDone = isDone,
+    assignmentId = assignmentId
+  )
+
   private def tested(
       taskService: TaskService[IO] = mock,
-      assignmentService: AssignmentService[IO] = mock,
+      assignmentService: AssignmentService[IO] = mock
   )(using
       getTaskResponseMapper: GetTaskResponseMapper,
       taskEntityMapper: TaskEntityMapper
   ): TaskGateway[IO] =
     TaskGateway.make(
       taskService = taskService,
-      assignmentService = assignmentService,
+      assignmentService = assignmentService
     )
